99bb133f4f880eef2e54ba1a9668b82c41f69900
Łukasz Ferszt <33269100+mocano6@users.noreply.github.com>
2018-02-21 14:36:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//ruchome menu--------------------------
+let bar = document.querySelector('#bar');
+let content = document.querySelector('.content');
+
+
+bar.addEventListener('mouseover', ()=>{ 
+    for(let i=1; i<bar.children.length; i++){
+        bar.children[i].style.visibility = "visible";
+        bar.style.transition = "all 1s";
+    };
+    bar.style.minWidth = "250px";
+    bar.firstElementChild.style.display = "none";
+    content.style.paddingLeft = "250px";
+});
+bar.addEventListener('mouseout', ()=>{
+    
+    bar.style.width = "50px";
+    bar.style.minWidth = "50px";
+    bar.style.backgroundColor = "rgba(0, 0, 0, 0.00)";
+    for(let i=1; i<bar.children.length; i++){
+        bar.children[i].style.visibility = "hidden";
+        bar.style.transition = "all 1s linear";
+    }
+    bar.firstElementChild.style.display = "block";
+    content.style.paddingLeft = "50px";
+});
+//-----------------------portfolio menu
